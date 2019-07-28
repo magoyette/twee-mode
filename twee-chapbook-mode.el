238559cda36f4698a56ec95e3e0d69f75c0e612b
magoyette <goyette.marcandre@gmail.com>
2019-07-28 17:17:14 -0400

    Add decent syntax highlight only with single-line regex
    Code needs to be cleaned and there'll be some false positives
    unless functions are used for the syntax highlight.

--- twee-chapbook-mode.el
+++ twee-chapbook-mode.el
@@ -27,6 +27,8 @@
 
 ;;; Code:
 
+(require 'font-lock)
+
 (defconst twee-chapbook-mode--syntax-table
   (let ((table (make-syntax-table)))
 
@@ -47,6 +49,47 @@
             (cdr bounds)
             twee-chapbook-mode--keywords))))
 
+(defvar twee-chapbook-mode-passage-header-face 'twee-chapbook-mode-passage-header-face
+  "Face for Markdown headers in an OpenAPI YAML file.")
+
+(defface twee-chapbook-mode-passage-header-face
+  '((t (:inherit font-lock-type-face :weight bold)))
+  "Face for passage headers in a Twee Chapbook file."
+  :group 'faces)
+
+(defconst twee-chapbook-mode--variable-init-regex
+  "^\\([$_A-Za-z][$_A-Za-z0-9.]*\\)\\(?::\\|\s(.*?):\\)\s")
+
+(defconst twee-chapbook-mode--parameter-name-regex
+  "\s+\\([$_A-Za-z][$_A-Za-z0-9.]*\\):\s")
+
+(defun twee-chapbook-mode--build-string-regex (delimiter)
+  "Build a regex that match a string for the provided DELIMITER."
+  (format "\\(%s\\(?:[^%s\\]+\\|\\\\\\(?:.\\|\\)\\)*%s\\)"
+          delimiter delimiter delimiter))
+
+(defun twee-chapbook-mode--build-string-value-regex (delimiter)
+  "Build a regex for a string value within DELIMITER."
+  (format "%s%s"
+          twee-chapbook-mode--parameter-name-regex
+          (twee-chapbook-mode--build-string-regex delimiter)))
+
+(defun twee-chapbook-mode--build-string-constant-regex (delimiter)
+  "Build a regex for a string value within DELIMITER."
+  (format "\\(,\s+\\|\\[\\|==\s+\\|if\\(?:always\\|never\\)?\s+\\|unless\s+\\|\\(?:link\\|menu\\|input\\)\sfor\s\\)%s"
+          (twee-chapbook-mode--build-string-regex delimiter)))
+
+(defun twee-chapbook-mode--build-variable-string-value-regex (delimiter)
+  "Build a regex for a string value within DELIMITER."
+  (format "%s%s"
+          twee-chapbook-mode--variable-init-regex
+          (twee-chapbook-mode--build-string-regex delimiter)))
+
+(defun twee-chapbook-mode--build-additional-parameter-name-regex ()
+  "Build a regex for a string value"
+  (format ",%s"
+          twee-chapbook-mode--parameter-name-regex))
+
 ;; font-lock-warning-face: error or warning
 ;; font-lock-function-name-face: name of a function
 ;; font-lock-variable-name-face: name of a variable
@@ -61,23 +104,31 @@
   `(
     ;;;;; Passages
 
-    ("^::[[:space:]]*\\(.*\\)$" 1 font-lock-type-face)
+    ("^::[[:space:]]*\\(.*\\)$" 1 twee-chapbook-mode-passage-header-face)
 
     ;;;;; Modifiers
 
     ;; Font lock rules for Modifiers must be applied before Links
 
     ;; Modifier without parameters
-    ("^[[]\\([^][\s]+\\)[]]$" 1 font-lock-function-name-face)
+    ("^[[]\\([^][\s]+\\)\\(\s+[^][]+\\)?[]]$" 1 font-lock-function-name-face)
 
-    ;; Modifier with parameters
-    ("^[[]\\([^][\s]+\\)\s+\\([^][]+\\)[]]$"
-     (1 font-lock-function-name-face t)
-     (2 font-lock-variable-name-face t))
+    ;; Modifier without parameters
+    ("^[[]\\([$_A-Za-z][$_A-Za-z0-9.]*\\)\s+\\([$_A-Za-z0-9.\s]+\\)[]]$" 2 font-lock-function-name-face)
+
+    ;; Booleans modifiers
+    ("^[[]\\(?:if\\(?:always\\|never\\)?\\|unless\\)\s+!?\\([$_A-Za-z][$_A-Za-z0-9.]*\\)\\([]]$\\|;\\)" 1 font-lock-variable-name-face t)
+
+    ("\\([!=]==\\|>=?\\|<=?\\)\s+\\([$_A-Za-z][$_A-Za-z0-9.]*\\)" 2 font-lock-variable-name-face t)
+    ("\\([$_A-Za-z][$_A-Za-z0-9.]*\\)\s+\\([!=]==\\|>=?\\|<=?\\)" 1 font-lock-variable-name-face t)
+
+    ;; Additional modifier after a ;
+    ("^[[][^;]*;\s+\\([$_A-Za-z0-9.]+\\).*?[]]$" 1 font-lock-function-name-face t)
 
     ;;;;; Links
 
     ;; LinkText->PassageNameOrUrl
+    ;; or Twine 1's LinkText|PassageNameOrUrl
     ("[[][[]\\([^]]*?\\)\\(->\\||\\)\\([^]]*\\)[]][]]"
      (1 font-lock-string-face t)
      (3 font-lock-type-face t))
@@ -87,31 +138,53 @@
      (1 font-lock-type-face t)
      (2 font-lock-string-face))
 
-    ;; LinkTextIsEqualToPassageName [Must be last in priority]
+    ;; LinkTextIsEqualToPassageName [Must be last in priority among font lock rules for links]
     ("[[][[]\\([^]]*\\)[]][]]" 1 font-lock-type-face)
 
     ;;;;; Variables
 
     ;; Variable statement
-    ("^\\([$_A-Za-z][$_A-Za-z0-9.]*\\):\s" 1 font-lock-variable-name-face)
+    (,(format "%s" twee-chapbook-mode--variable-init-regex) 1 font-lock-variable-name-face)
 
-    ;; String in variable statement
-    ;; Regex for strings with support for quoting with \ are quite horrible
-    ;; The 't' at the end is necessary to support properly strings like 'A string "value"'
-    ("^[$_A-Za-z][$_A-Za-z0-9.]*:.*?\\(\"\\(\\(?:[^\"\\]+\\|\\\\\\(?:.\\|\\)\\)*\\)\"\\)" 1 font-lock-string-face t)
-    ("^[$_A-Za-z][$_A-Za-z0-9.]*:.*?\\('\\(\\(?:[^'\\]+\\|\\\\\\(?:.\\|\\)\\)*\\)'\\)" 1 font-lock-string-face t)
-
-    ;; Reference to a variable inside a string in the context of a variable statement
-    ;; The 't' at the end allows to apply the syntax highlight on top of the string syntax highlight
-    ("^[$_A-Za-z][$_A-Za-z0-9.]*:.*?\\({[$_A-Za-z][$_A-Za-z0-9.]*}\\)" 1 font-lock-variable-name-face t)
+    ;; Variable statement initialized to another variable
+    (,(format "%s\\([$_A-Za-z][$_A-Za-z0-9.]*\\)$" twee-chapbook-mode--variable-init-regex) 2 font-lock-variable-name-face)
 
     ;; Reference to a variable
     ("\\({[$_A-Za-z][$_A-Za-z0-9.]*}\\)" 1 font-lock-variable-name-face)
 
-    ;; Variables in inserts
-    ("{[^}:]*?\\([$_A-Za-z][$_A-Za-z0-9]*\\):\s[^}]*}" 1 font-lock-variable-name-face)
+    (,(twee-chapbook-mode--build-variable-string-value-regex "\"")
+     (2 font-lock-string-face t))
+    (,(twee-chapbook-mode--build-variable-string-value-regex "'")
+     (2 font-lock-string-face t))
 
-    ("{\\([^}]*\\)}" 1 font-lock-comment-face)
+    (,(twee-chapbook-mode--build-string-constant-regex "\"")
+     (2 font-lock-string-face t))
+    (,(twee-chapbook-mode--build-string-constant-regex "'")
+     (2 font-lock-string-face t))
+
+    ;; Reference to a variable inside a string in the context of a variable statement
+    ;; Must be after the calls to twee-chapbook-mode--build-variable-string-value-regex
+    ("^[$_A-Za-z][$_A-Za-z0-9.]*:.*?\\({[$_A-Za-z][$_A-Za-z0-9.]*}\\)" 1 font-lock-variable-name-face t)
+
+    ;;;;; Inserts
+
+    ;; Highlight the start of the insert like a function name
+    ("{\\([^},:'\"\n]*\\)[^}]*}" 1 font-lock-function-name-face)
+
+    ;; Highlight true and false
+    ("\\(&&\\|||\\|==\\|:\\)\s+!?\\(true\\|false\\)" 2 font-lock-constant-face t)
+    ("\\(true\\|false\\)\\(\s+==\\|\s*\\|\s+||\\|\s+&&]\\)" 1 font-lock-constant-face t)
+
+    (,(twee-chapbook-mode--build-additional-parameter-name-regex)
+     (1 font-lock-variable-name-face t))
+
+    ;; Parameter string value in inserts
+    (,(twee-chapbook-mode--build-string-value-regex "\"")
+     (1 font-lock-variable-name-face t)
+     (2 font-lock-string-face t))
+    (,(twee-chapbook-mode--build-string-value-regex "'")
+     (1 font-lock-variable-name-face t)
+     (2 font-lock-string-face t))
     ))
 
 (defalias 'twee-chapbook-parent-mode
